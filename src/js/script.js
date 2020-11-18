641cb005736af77fa3d59fce19253b4451155d0e
Stanislav <stasberestetskiy@gmail.com>
2020-11-18 13:26:26 +0200

    carousel is done

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   slidesToShow: 1,
   slidesToScroll: 1,
   speed: 1200,
-  prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="./src/icons/left.svg"></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="./src/icons/right.svg"></button>',
     });
 });
